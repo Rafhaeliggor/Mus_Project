1d33e4449ec75484c57ee344d05d0743698d38f6
Rafhael <rafhaeliggor@gmail.com>
2024-04-14 20:31:09 -0300

    A.1

--- Test/Drawings.py
+++ Test/Drawings.py
@@ -13,10 +13,23 @@ class App(ctk.CTk):
         self.bind("<Escape>", lambda event: self.quit())
                 
         def create_oval_test():
+            #x_oval = 140, y_oval = 100, x_line = 15, y_line = 250
+
+            #custom zx,zy = center of oval
+            #oval ((x1,y1, x2, y2) -> ((zx-70, zy-50, zx+70, zy+50))
+            #line ((zx+15,zy+250,zx+70,zy))
+
             #Create size and position customizabable
-            print("entrou na função")
-            screenTest.create_oval((110, 450, 250, 350), fill="black")
-            screenTest.create_rectangle((250,150, 235, 400), fill="black")
+            zx = 200
+            zy = 300
+            t = 4
+
+            screenTest.create_oval((zx-7*t, zy-5*t, zx+7*t, zy+5*t), fill="black")
+            #screenTest.create_aa_circle(x_pos=zx, y_pos=zy, radius=3, fill="red")
+            screenTest.create_rectangle((zx+5*t,zy-25*t,zx+7*t,zy), fill="black")
+
+            #screenTest.create_oval((110, 450, 250, 350), fill="black")
+            #screenTest.create_rectangle((250,150, 235, 400), fill="black")
             
         #Canvas
         
